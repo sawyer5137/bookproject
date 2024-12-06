cd6bcaa3386974234ee84dbd890a67ef5196f05f
sawyer5137@gmail <sawyer5137@gmail.com>
2024-12-05 19:09:14 -0600

    improved home page

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -52,7 +52,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
-app.post("/logout", (req, res) => {
+router.post("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ message: "Logout failed" });
